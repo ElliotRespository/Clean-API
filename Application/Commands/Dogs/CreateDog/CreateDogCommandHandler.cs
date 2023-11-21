879cb32fc80e20564b99ce9d1849d3705fc007a7
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2023-11-21 20:40:41 +0100

    ändrade query till command då query är fel för att posta updatera eller delete då ska det vara command

---
+++ Application/Commands/Dogs/CreateDog/CreateDogCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+
+namespace Application.Commands.Dogs.CreateDog
+{
+    public class CreateDogCommandHandler : IRequestHandler<CreateDogCommand, Dog>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public Task<Dog> Handle(CreateDogCommand request, CancellationToken cancellationToken)
+        {
+            var newDog = new Dog { Name = request.Name, animalID = Guid.NewGuid() };
+            _mockDatabase.allDogs.Add(newDog);
+            return Task.FromResult(newDog);
+        }
+    }
+}
