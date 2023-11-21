0b040258a5207246623656f5868e192911489d6b
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2023-11-21 18:03:11 +0100

    lagt till createdog för post

---
+++ Application/Querys/Dogs/CreateDog/CreateDogQuery.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+using MediatR;
+
+
+namespace Application.Querys.Dogs.CreateDog
+{
+    public class CreateDogQuery : IRequest<Dog>
+    {
+        public string Name { get; set; }
+
+    }
+}
