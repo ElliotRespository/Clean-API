f5a4bc2d3fbcedada7fafdbed3c8e6903047bdcb
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2023-12-20 18:35:57 +0100

    jag har lagt till many to many relation

--- Infrastructure/Database/SqlDataBases/RealDatabase.cs
+++ Infrastructure/Database/SqlDataBases/RealDatabase.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models.Animalmodels;
+﻿using Domain.Models;
+using Domain.Models.Animalmodels;
 using Domain.Models.UserModels;
 using Infrastructure.Database.DataBaseSeed;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +24,8 @@ namespace Infrastructure.Database.SqlDataBases
 
         public virtual DbSet<UserModel> Users { get; set; }
 
+        public virtual DbSet<UserAnimal> UserAnimals { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS; Database=CleanApi_demo_db; Trusted_Connection=true; TrustServerCertificate=true;");
@@ -31,7 +34,19 @@ namespace Infrastructure.Database.SqlDataBases
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            //Configuring the many-to-many relationship
+            modelBuilder.Entity<UserAnimal>()
+                .HasKey(ua => new { ua.UserId, ua.AnimalId });
 
+            modelBuilder.Entity<UserAnimal>()
+                .HasOne(ua => ua.user)
+                .WithMany(ua => ua.UserAnimals)
+                .HasForeignKey(ua => ua.UserId);
+
+            modelBuilder.Entity<UserAnimal>()
+                .HasOne(ua => ua.Animal)
+                .WithMany(a => a.UserAnimals)
+                .HasForeignKey(ua => ua.AnimalId);
             DataBaseSeedHelper.SeedData(modelBuilder);
         }
 
